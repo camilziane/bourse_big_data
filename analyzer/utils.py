1e4da9c484632e6286bb929347189af8878a5db1
Camil Ziane <camilbel@live.fr>
2024-03-25 21:19:00 +0100

    fix(analyze): utils

--- analyzer/utils.py
+++ analyzer/utils.py
@@ -1,12 +1,12 @@
 from concurrent.futures import ThreadPoolExecutor
 import multiprocessing
 import pandas as pd
-from analyze import store_file
+from analyze import read_file
 from tqdm import tqdm
 
 
 
-def multi_read_df_from_paths(paths: list[str], read_function = store_file) -> list[pd.DataFrame]:
+def multi_read_df_from_paths(paths: list[str], read_function = read_file) -> list[pd.DataFrame]:
     dfs = []
     num_cpus = multiprocessing.cpu_count()
     with ThreadPoolExecutor(max_workers=num_cpus) as executor:
