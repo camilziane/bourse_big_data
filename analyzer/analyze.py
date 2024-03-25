690c9696669d09a0b280c02d23a7cb8909010782
Camil Ziane <camilbel@live.fr>
2024-03-26 00:40:59 +0100

    fix(analyze): astype str df['last']

--- analyzer/analyze.py
+++ analyzer/analyze.py
@@ -72,7 +72,7 @@ def get_files_infos_df(backup_path:Optional[str]=None) -> pd.DataFrame:
 def read_file(path: str) -> pd.DataFrame:
     df: pd.DataFrame = pd.read_pickle(path) 
     df['last_suffix'] = df['last'].str.extract(r'\((.*?)\)', expand=False)
-    df['last'] = df['last'].str.replace(r'\((.*?)\)', '', regex=True).str.replace(' ', '').astype(float)
+    df['last'] = df['last'].astype(str).str.replace(r'\((.*?)\)', '', regex=True).str.replace(' ', '').astype(float)
     df.attrs =  {'timestamp': " ".join(path.split(" ")[1:]).split(".")[0]}
     return df
 
