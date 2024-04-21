25422bf1fde546b56fad4f942b57e29199329afe
eithan <eithan.nakache@epita.fr>
2024-04-21 17:10:35 +0200

    feat(dashboard) : add settings popup and fix log button , now the first stock is the target , the other ones will be only display ass scatter points to compare with the main one

--- dashboard/bourse.py
+++ dashboard/bourse.py
@@ -1,24 +1,20 @@
 import dash
-from dash import dcc
-from dash import html
+from dash import dcc ,html
 import dash.dependencies as ddep
+from dash.dependencies import Input, Output, State
+import dash_bootstrap_components as dbc
 import pandas as pd
 import sqlalchemy
 from lib.constant import IS_DOCKER
 from typing import Optional , Tuple
-from dataclasses import dataclass
 from datetime import date, timedelta, datetime
 import plotly.graph_objects as go
 import plotly.express as px
 import plotly.subplots as ms
 
-@dataclass
-class Company:
-    id: int
-    name: str
-
 
 # external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
 
 DATABASE_URI = (
     "timescaledb://ricou:monmdp@db:5432/bourse"
@@ -28,27 +24,70 @@ DATABASE_URI = (
 engine = sqlalchemy.create_engine(DATABASE_URI)
 
 app = dash.Dash(
-    __name__, title="Bourse", suppress_callback_exceptions=True
+    __name__, title="Bourse", suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.BOOTSTRAP]
 )  # , external_stylesheets=external_stylesheets)
+
 server = app.server
 companies = pd.read_sql_query("SELECT * FROM companies", engine)
 companies_options = [
     {"label": row["name"], "value":row["id"]} for _, row in companies.iterrows()
 ]
 companies_id_to_labels = {row["id"]: row["name"] for _, row in companies.iterrows()}
+
+modal = dbc.Modal(
+    [
+        dbc.ModalHeader(dbc.ModalTitle("Adjust Chart Settings")),
+        dbc.ModalBody(
+            dbc.Form([
+                dbc.Switch(id="switch-log", label="Logarithmic", value=False),
+                dbc.Switch(id="switch-volume", label="Volume", value=False),
+                dbc.Switch(id="switch-bollinger-bands", label="Bollinger Bands", value=False),
+                html.Div(
+                    [
+                        dbc.Label("Bollinger Bands Window Size:"),
+                        dbc.Input(type="number", id="bollinger-window", min=1, step=1, value=20, disabled=True)
+                    ],
+                    id="bollinger-window-container",
+                    style={'display': 'none'} 
+                )
+            ])
+        ),
+        dbc.ModalFooter(
+            dbc.Button("Close", id="close-modal", className="ms-auto", n_clicks=0)
+        ),
+    ],
+    id="modal-settings",
+    is_open=False
+)
+
+
+@app.callback(
+    [Output("bollinger-window-container", "style"),
+     Output("bollinger-window", "disabled")],
+    [Input("switch-bollinger-bands", "value")]
+)
+def toggle_bollinger_window(switch_value):
+    if switch_value:
+        return {'display': 'block'}, False 
+    else:
+        return {'display': 'none'}, True 
+
+@app.callback(
+    Output("modal-settings", "is_open"),
+    [Input("open-settings", "n_clicks"), Input("close-modal", "n_clicks")],
+    [State("modal-settings", "is_open")]
+)
+def toggle_modal(n1, n2, is_open):
+    if n1 or n2:
+        return not is_open
+    return is_open
+
+open_modal_button = dbc.Button("Open Settings", id="open-settings", n_clicks=0)
+
 app.layout = html.Div(
     [
-        # dcc.Textarea(
-        #     id="sql-query",
-        #     value="""
-        #                 SELECT * FROM pg_catalog.pg_tables
-        #                     WHERE schemaname != 'pg_catalog' AND
-        #                           schemaname != 'information_schema';
-        #             """,
-        #     style={"width": "100%", "height": 100},
-        # ),
-        # html.Button("Execute", id="execute-query", n_clicks=0),
-        # html.Div(id="query-result"),
+        open_modal_button,
+        modal,
         dcc.DatePickerRange(
             id="my-date-picker-range",
             min_date_allowed=date(2019, 1, 1),
@@ -60,36 +99,49 @@ app.layout = html.Div(
         html.Div(id="output-container-date-picker-range"),
         dcc.Dropdown(companies_options, value=[4534], id="companies-dropdown", multi=True),
         dcc.RadioItems(["Candle Stick", "Line"], "Line", id="chart-type"),
-        dcc.Checklist(["Logarithmic"], [False], id="log-y"),
-        # html.Div(id="companies-output-container"),
         dcc.Graph(id="graph",style={'width': '90%', 'height': '90vh'}),
         html.Div(id="table-container"),
     ]
 )
 
 
-# @app.callback(
-#     ddep.Output("query-result", "children"),
-#     ddep.Input("execute-query", "n_clicks"),
-#     ddep.State("sql-query", "value"),
-# )
-# def run_query(n_clicks, query):
-#     if n_clicks > 0:
-#         try:
-#             result_df = pd.read_sql_query(query, engine)
-#             return html.Pre(result_df.to_string())
-#         except Exception as e:
-#             return html.Pre(str(e))
-#     return "Enter a query and press execute."
+def create_candlestick_trace(df, label):
+    return go.Candlestick(
+        x=df["date"],
+        open=df["open"],
+        high=df["high"],
+        low=df["low"],
+        close=df["close"],
+        name=f"{label}"
+    )
+
+def create_bands_trace(df):
+    upper_trace = go.Scatter(x=df['date'], y=df['Upper'], mode='lines', name='Bande Supérieure', line=dict(color='rgba(0,0,255,0.2)'))
+    lower_trace = go.Scatter(x=df['date'], y=df['Lower'], mode='lines', name='Bande Inférieure', line=dict(color='rgba(255,0,0,0.2)'))
+    zone_trace = go.Scatter(x=df['date'].tolist() + df['date'].tolist()[::-1],
+                            y=df['Upper'].tolist() + df['Lower'].tolist()[::-1],
+                            fill='toself', fillcolor='rgba(0,0,255,0.1)', line=dict(color='rgba(255,255,255,0)'),
+                            name='Zone entre les bandes')
+    return upper_trace, lower_trace, zone_trace
+
+def create_volume_trace(df, label):
+    df['change'] = df['close'] - df['open']
+    colors = ['green' if change > 0 else 'red' for change in df['change']]
+    return go.Bar(x=df["date"], y=df["volume"], marker_color=colors, name=f"Volume {label}")
 
 
 @app.callback(
-    ddep.Output("graph", "figure"),
-    ddep.Input("companies-dropdown", "value"),
-    ddep.Input("my-date-picker-range", "start_date"),
-    ddep.Input("my-date-picker-range", "end_date"),
-    ddep.Input("chart-type", "value"),
-    ddep.Input("log-y", "value"),
+    Output("graph", "figure"),
+    [
+        Input("companies-dropdown", "value"),
+        Input("my-date-picker-range", "start_date"),
+        Input("my-date-picker-range", "end_date"),
+        Input("chart-type", "value"),
+        Input("switch-log", "value"), 
+        Input("switch-volume", "value"), 
+        Input("switch-bollinger-bands", "value"),
+        Input("bollinger-window", "value")
+    ]
 )
 def update_companies_chart(
     values: Optional[list[int]],
@@ -97,9 +149,16 @@ def update_companies_chart(
     end_date: Optional[str],
     chart_type: str,
     log_y: bool,
+    volume: bool,
+    bollinger_bands: bool,
+    bollinger_window: Optional[int]
 ):
     
-    fig = ms.make_subplots(rows=len(values)+1, cols=1, shared_xaxes=True, vertical_spacing=0.1)
+    if volume:
+        fig = ms.make_subplots(rows=2, cols=1, shared_xaxes=True, vertical_spacing=0.1)
+    else:
+        fig = ms.make_subplots(rows=1, cols=1, shared_xaxes=True, vertical_spacing=0.1)
+
 
     if not values:
         return go.Figure()
@@ -108,7 +167,7 @@ def update_companies_chart(
     end_date = str(datetime.fromisoformat(end_date) + timedelta(days=1))
     if chart_type == 'Candle Stick':
         
-        for value in values:
+        for i,value in enumerate(values):
             label = companies_id_to_labels.get(value, '')
             
             df = pd.read_sql_query(
@@ -116,67 +175,53 @@ def update_companies_chart(
                 engine,
             )
             
-            window = 5  # Parametre qui doit etre modifiable
-            df['MA'] = df['close'].rolling(window=window).mean()
-            df['STD'] = df['close'].rolling(window=window).std()
-            df['Upper'] = df['MA'] + (df['STD'] * 2)
-            df['Lower'] = df['MA'] - (df['STD'] * 2)
-                        
-            fig.add_trace(
-                go.Candlestick(
-                    x=df["date"],
-                    open=df["open"],
-                    high=df["high"],
-                    low=df["low"],
-                    close=df["close"],
-                    name=f"{label}"
-                )
-            )
-            
-            fig.add_trace(go.Scatter(x=df['date'], y=df['Upper'], mode='lines', name='Bande Supérieure', line=dict(color='rgba(0,0,255,0.2)')))
-            fig.add_trace(go.Scatter(x=df['date'], y=df['Lower'], mode='lines', name='Bande Inférieure', line=dict(color='rgba(255,0,0,0.2)')))
-            
-            fig.add_trace(go.Scatter(x=df['date'].tolist() + df['date'].tolist()[::-1],
-                         y=df['Upper'].tolist() + df['Lower'].tolist()[::-1],
-                         fill='toself', fillcolor='rgba(0,0,255,0.1)', line=dict(color='rgba(255,255,255,0)'),
-                         name='Zone entre les bandes'))
+            if i == 0:            
+                fig.add_trace(create_candlestick_trace(df, label))
+            else:
+                fig.add_trace(go.Scatter(x=df["date"], y=df["close"], mode='lines', name=f"{label}"))
+                
+            if bollinger_bands and i == 0:
+                df['MA'] = df['close'].rolling(window=bollinger_window).mean()
+                df['STD'] = df['close'].rolling(window=bollinger_window).std()
+                df['Upper'] = df['MA'] + (df['STD'] * 2)
+                df['Lower'] = df['MA'] - (df['STD'] * 2)
+                upper_trace, lower_trace, zone_trace = create_bands_trace(df)
+                fig.add_trace(upper_trace)
+                fig.add_trace(lower_trace)
+                fig.add_trace(zone_trace)
+        
         
     else:
-        for value in values:
+        for i,value in enumerate(values):
             label = companies_id_to_labels.get(value, '')
             df = pd.read_sql_query(
                 f"SELECT * FROM daystocks WHERE cid = {value} and date >= '{start_date}' and date < '{end_date}' ORDER by date",
                 engine,
             )
-            
-            window = 20  # Parametre qui doit etre modifiable
-            df['MA'] = df['close'].rolling(window=window).mean()
-            df['STD'] = df['close'].rolling(window=window).std()
-            df['Upper'] = df['MA'] + (df['STD'] * 2)
-            df['Lower'] = df['MA'] - (df['STD'] * 2)
             
             fig.add_trace(go.Scatter(x=df["date"], y=df["close"], mode='lines', name=f"{label}"))
             
-            fig.add_trace(go.Scatter(x=df['date'], y=df['Upper'], mode='lines', name='Bande Supérieure', line=dict(color='rgba(0,0,255,0.2)')))
-            fig.add_trace(go.Scatter(x=df['date'], y=df['Lower'], mode='lines', name='Bande Inférieure', line=dict(color='rgba(255,0,0,0.2)')))
-            
-            fig.add_trace(go.Scatter(x=df['date'].tolist() + df['date'].tolist()[::-1],
-                         y=df['Upper'].tolist() + df['Lower'].tolist()[::-1],
-                         fill='toself', fillcolor='rgba(0,0,255,0.1)', line=dict(color='rgba(255,255,255,0)'),
-                         name='Zone entre les bandes'))
+            if bollinger_bands and i == 0:
+                df['MA'] = df['close'].rolling(window=bollinger_window).mean()
+                df['STD'] = df['close'].rolling(window=bollinger_window).std()
+                df['Upper'] = df['MA'] + (df['STD'] * 2)
+                df['Lower'] = df['MA'] - (df['STD'] * 2)
+                upper_trace, lower_trace, zone_trace = create_bands_trace(df)
+                fig.add_trace(upper_trace)
+                fig.add_trace(lower_trace)
+                fig.add_trace(zone_trace)
     
     if log_y:
         fig.update_yaxes(type='log')
         
-    for i,value in enumerate(values):
+    if volume:
+        value = values[0]
         label = companies_id_to_labels.get(value, '')
         df = pd.read_sql_query( f"SELECT * FROM daystocks WHERE cid = {value} and date >= '{start_date}' and date < '{end_date}' ORDER by date", engine)
-        df['change'] = df['close'] - df['open']
-        colors = ['green' if change > 0 else 'red' for change in df['change']]
-        fig.add_trace(go.Bar(x=df["date"], y=df["volume"],marker_color=colors) , row=2+i, col=1)
-        fig.update_yaxes(title_text=f"Volume {label}", row=2+i, col=1)
+        fig.add_trace(create_volume_trace(df, label), row=2, col=1)
+        fig.update_yaxes(title_text=f"Volume {label}", row=2, col=1)
 
-    
+        
     fig.update_layout(xaxis_rangeslider_visible=False,
 
                        title={
