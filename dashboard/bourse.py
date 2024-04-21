a71078e086bef9fe10f76fd36ae7c037a4d80ef2
eithan <eithan.nakache@epita.fr>
2024-04-21 02:14:47 +0200

    feat(dashboard): add basics functionalities

--- dashboard/bourse.py
+++ dashboard/bourse.py
@@ -5,11 +5,17 @@ import dash.dependencies as ddep
 import pandas as pd
 import sqlalchemy
 from lib.constant import IS_DOCKER
-from typing import Optional
+from typing import Optional , Tuple
+from dataclasses import dataclass
 from datetime import date, timedelta, datetime
 import plotly.graph_objects as go
 import plotly.express as px
+import plotly.subplots as ms
 
+@dataclass
+class Company:
+    id: int
+    name: str
 
 
 # external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -27,8 +33,9 @@ app = dash.Dash(
 server = app.server
 companies = pd.read_sql_query("SELECT * FROM companies", engine)
 companies_options = [
-    {"label": row["name"], "value": row["id"]} for _, row in companies.iterrows()
+    {"label": row["name"], "value":row["id"]} for _, row in companies.iterrows()
 ]
+companies_id_to_labels = {row["id"]: row["name"] for _, row in companies.iterrows()}
 app.layout = html.Div(
     [
         # dcc.Textarea(
@@ -47,14 +54,15 @@ app.layout = html.Div(
             min_date_allowed=date(2019, 1, 1),
             max_date_allowed=date(2023, 12, 31),
             initial_visible_month=date(2019, 1, 1),
+            start_date=date(2019, 1, 1),
             end_date=date(2023, 12, 31),
         ),
         html.Div(id="output-container-date-picker-range"),
-        dcc.Dropdown(companies_options, 4534, id="companies-dropdown"),
+        dcc.Dropdown(companies_options, value=[4534], id="companies-dropdown", multi=True),
         dcc.RadioItems(["Candle Stick", "Line"], "Line", id="chart-type"),
         dcc.Checklist(["Logarithmic"], [False], id="log-y"),
         # html.Div(id="companies-output-container"),
-        dcc.Graph(id="graph"),
+        dcc.Graph(id="graph",style={'width': '90vh', 'height': '90vh'}),
     ]
 )
 
@@ -83,44 +91,101 @@ app.layout = html.Div(
     ddep.Input("log-y", "value"),
 )
 def update_companies_chart(
-    value: Optional[int],
+    values: Optional[list[int]],
     start_date: Optional[str],
     end_date: Optional[str],
     chart_type: str,
     log_y: bool,
 ):
+    
+    fig = ms.make_subplots(rows=len(values)+1, cols=1, shared_xaxes=True, vertical_spacing=0.1)
 
-    if not value:
+    if not values:
         return go.Figure()
     if not start_date or not end_date:
         return go.Figure()
     end_date = str(datetime.fromisoformat(end_date) + timedelta(days=1))
     if chart_type == 'Candle Stick':
-        df = pd.read_sql_query(
-            f"SELECT * FROM daystocks WHERE cid = {value} and date >= '{start_date}' and date < '{end_date}' ORDER by date",
-            engine,
-        )
-        fig = go.Figure(
-            go.Candlestick(
-                x=df["date"],
-                open=df["open"],
-                high=df["high"],
-                low=df["low"],
-                close=df["close"],
+        
+        for value in values:
+            label = companies_id_to_labels.get(value, '')
+            
+            df = pd.read_sql_query(
+                f"SELECT * FROM daystocks WHERE cid = {value} and date >= '{start_date}' and date < '{end_date}' ORDER by date",
+                engine,
             )
             
-        )
-    
+            window = 20  # Parametre qui doit etre modifiable
+            df['MA'] = df['close'].rolling(window=window).mean()
+            df['STD'] = df['close'].rolling(window=window).std()
+            df['Upper'] = df['MA'] + (df['STD'] * 2)
+            df['Lower'] = df['MA'] - (df['STD'] * 2)
+                        
+            fig.add_trace(
+                go.Candlestick(
+                    x=df["date"],
+                    open=df["open"],
+                    high=df["high"],
+                    low=df["low"],
+                    close=df["close"],
+                    name=f"{label}"
+                )
+            )
+            
+            fig.add_trace(go.Scatter(x=df['date'], y=df['Upper'], mode='lines', name='Bande Supérieure', line=dict(color='rgba(0,0,255,0.2)')))
+            fig.add_trace(go.Scatter(x=df['date'], y=df['Lower'], mode='lines', name='Bande Inférieure', line=dict(color='rgba(255,0,0,0.2)')))
+            
+            fig.add_trace(go.Scatter(x=df['date'].tolist() + df['date'].tolist()[::-1],
+                         y=df['Upper'].tolist() + df['Lower'].tolist()[::-1],
+                         fill='toself', fillcolor='rgba(0,0,255,0.1)', line=dict(color='rgba(255,255,255,0)'),
+                         name='Zone entre les bandes'))
+        
     else:
-        df = pd.read_sql_query(
-            f"SELECT * FROM stocks WHERE cid = {value} and date >= '{start_date}' and date < '{end_date}' ORDER by date",
-            engine,
-        )
-        fig = px.line(df, x=df["date"], y=df["value"])
+        for value in values:
+            label = companies_id_to_labels.get(value, '')
+            df = pd.read_sql_query(
+                f"SELECT * FROM daystocks WHERE cid = {value} and date >= '{start_date}' and date < '{end_date}' ORDER by date",
+                engine,
+            )
+            
+            window = 20  # Parametre qui doit etre modifiable
+            df['MA'] = df['close'].rolling(window=window).mean()
+            df['STD'] = df['close'].rolling(window=window).std()
+            df['Upper'] = df['MA'] + (df['STD'] * 2)
+            df['Lower'] = df['MA'] - (df['STD'] * 2)
+            
+            fig.add_trace(go.Scatter(x=df["date"], y=df["close"], mode='lines', name=f"{label}"))
+            
+            fig.add_trace(go.Scatter(x=df['date'], y=df['Upper'], mode='lines', name='Bande Supérieure', line=dict(color='rgba(0,0,255,0.2)')))
+            fig.add_trace(go.Scatter(x=df['date'], y=df['Lower'], mode='lines', name='Bande Inférieure', line=dict(color='rgba(255,0,0,0.2)')))
+            
+            fig.add_trace(go.Scatter(x=df['date'].tolist() + df['date'].tolist()[::-1],
+                         y=df['Upper'].tolist() + df['Lower'].tolist()[::-1],
+                         fill='toself', fillcolor='rgba(0,0,255,0.1)', line=dict(color='rgba(255,255,255,0)'),
+                         name='Zone entre les bandes'))
+    
     if log_y:
         fig.update_yaxes(type='log')
-    fig.update_layout(xaxis_rangeslider_visible=False)
+        
+    for i,value in enumerate(values):
+        label = companies_id_to_labels.get(value, '')
+        df = pd.read_sql_query( f"SELECT * FROM daystocks WHERE cid = {value} and date >= '{start_date}' and date < '{end_date}' ORDER by date", engine)
+        df['change'] = df['close'] - df['open']
+        colors = ['green' if change > 0 else 'red' for change in df['change']]
+        fig.add_trace(go.Bar(x=df["date"], y=df["volume"],marker_color=colors) , row=2+i, col=1)
+        fig.update_yaxes(title_text=f"Volume {label}", row=2+i, col=1)
+
+    
+    fig.update_layout(xaxis_rangeslider_visible=False,
+
+                       title={
+                            'text': f"Daily chart",    
+                            'x': 0.5,
+                            'xanchor': 'center'
+                        }, 
+            )
     return fig
+
 
 if __name__ == "__main__":
     app.run(debug=True)
